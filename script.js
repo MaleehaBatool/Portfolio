c3497b8e6d63e5a69974ee1d8ef7498d4cb12a9a
Maleeha Batool <149329409+MaleehaBatool@users.noreply.github.com>
2026-02-22 17:21:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const text = "Full Stack Developer | AI Enthusiast | React & Node.js Specialist";
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+window.onload = typeEffect;
+
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add("show");
+        }
+    });
+});
